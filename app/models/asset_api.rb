dffdd155e56c28d5674472100ff9ef987dc7006c
msimon42 <msimon@uab.edu>
2019-12-30 23:19:21 -0600

    Add methods to query asset api

--- app/models/asset_api.rb
+++ app/models/asset_api.rb
@@ -3,7 +3,13 @@ require 'net/http'
 class AssetApi
   def self.get_all
     uri = URI("https://api.nomics.com/v1/currencies/ticker?key=2595a8427418356ea4954a8c2f0d2782")
-    response =  Net::HTTP.get(uri)
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
+
+  def self.get(asset)
+    uri = URI("https://api.nomics.com/v1/currencies/ticker?key=2595a8427418356ea4954a8c2f0d2782&ids=#{asset}")
+    response = Net::HTTP.get(uri)
     JSON.parse(response)
   end
 end
