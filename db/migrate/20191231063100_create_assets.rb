adb19c61f5abb47d42f2d035cb2afaa56fd1b042
msimon42 <msimon@uab.edu>
2019-12-31 00:32:16 -0600

    Add assets table to db

---
+++ db/migrate/20191231063100_create_assets.rb
@@ -0,0 +1,9 @@
+class CreateAssets < ActiveRecord::Migration[5.1]
+  def change
+    create_table :assets do |t|
+      t.string :symbol
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/assets.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :asset do
+    symbol { "MyString" }
+  end
+end
