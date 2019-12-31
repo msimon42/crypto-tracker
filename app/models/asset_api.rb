d18c810e6b36159f7be833b5ea5d652568d13556
msimon42 <msimon@uab.edu>
2019-12-31 15:24:37 -0600

    Add method to get metadata

--- app/models/asset_api.rb
+++ app/models/asset_api.rb
@@ -12,4 +12,10 @@ class AssetApi
     response = Net::HTTP.get(uri)
     JSON.parse(response)
   end
+
+  def self.get_metadata(asset)
+    uri = URI("https://api.nomics.com/v1/currencies?key=2595a8427418356ea4954a8c2f0d2782&ids=#{asset}")
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
 end
