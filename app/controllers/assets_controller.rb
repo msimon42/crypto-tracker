133fed800ddf4851bce69bba3abc5469a3ff858e
msimon42 <msimon@uab.edu>
2019-12-30 23:19:45 -0600

    Add to controller

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,10 @@
 class AssetsController < ApplicationController
   def index
-    @assets = AssetApi.get_all
+    assets = AssetApi.get_all
+    @paginatable_assets = Kaminari.paginate_array(assets).page(params[:page]).per(100)
+  end
+
+  def show
+    @asset = AssetApi.get(params[:name]).first
   end
 end
