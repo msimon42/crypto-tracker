c198ae5d6e3da1c923dbe96cd11f5421aed83e20
msimon42 <msimon@uab.edu>
2020-01-01 22:40:47 -0600

    Add test for bad password confirmation

--- spec/features/users/create_user_spec.rb
+++ spec/features/users/create_user_spec.rb
@@ -12,36 +12,50 @@ RSpec.describe 'when i fill in the user registration form', type: :feature do
   end
 
   it 'will create new user' do
-    fill_in :email, with: 'test1@gmail.org',
-    fill_in :username, with: 'user2',
+    fill_in :email, with: 'test1@gmail.org'
+    fill_in :username, with: 'user2'
     fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
 
-    click_on 'Register'
+    click_on 'Create Account'
 
-    expect(current_path).to eq('/dashboard')
+    expect(current_path).to eq('/assets')
     expect(page).to have_content('Welcome, user2!')
   end
 
   it 'will fail to create user if email is taken' do
-    fill_in :email, with: 'test@email.com',
-    fill_in :username, with: 'user2',
+    fill_in :email, with: 'test@email.com'
+    fill_in :username, with: 'user2'
     fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
 
-    click_on 'Register'
+    click_on 'Create Account'
 
     expect(current_path).to eq('/register')
-    expect(page).to have_content('Email already taken')
+    expect(page).to have_content('Email has already been taken')
   end
 
   it 'will fail to create user if username is taken' do
-    fill_in :email, with: 'test1@gmail.org',
-    fill_in :username, with: 'user1',
+    fill_in :email, with: 'test1@gmail.org'
+    fill_in :username, with: 'user1'
     fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
 
-    click_on 'Register'
+    click_on 'Create Account'
 
     expect(current_path).to eq('/register')
-    expect(page).to have_content('Username already taken')
+    expect(page).to have_content('Username has already been taken')
+  end
+
+  it 'will fail to create user if passwords do not match' do
+    fill_in :email, with: 'test1@gmail.org'
+    fill_in :username, with: 'user12'
+    fill_in :password, with: 'password'
+
+    click_on 'Create Account'
+
+    expect(current_path).to eq('/register')
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 
 end
