45989a4e7aeee41e60f57ebcae1c1c978035c61d
msimon42 <msimon@uab.edu>
2020-01-01 20:04:29 -0600

    Add users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+    new_user = User.create(user_params)
+    if new_user.save
+      flash[:success] = "Welcome, #{new_user.username}!"
+      session[:user_id] = new_user.id
+      redirect_to '/dashboard'
+    else
+      flash[:danger] = new_user.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :username, :password, :password_confirmation)
+  end
+end
