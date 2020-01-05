0238e87cda4099036e1509933bb880a8d46c966d
msimon42 <msimon@uab.edu>
2020-01-04 22:08:58 -0700

    Add method that returns array of symbols matching search query

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,8 @@
 class Asset < ApplicationRecord
   has_many :user_assets
   has_many :users, through: :user_assets
+
+  def self.find_symbols(query)
+    where('name LIKE ?', "%#{query.upcase}%").pluck(:symbol)
+  end
 end
