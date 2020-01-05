498e04324da3f390b64d1c87b3b795a2a9df282c
msimon42 <msimon@uab.edu>
2020-01-05 00:18:31 -0700

    Add helper methods

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,4 +5,24 @@ class Asset < ApplicationRecord
   def self.find_symbols(query)
     where('name LIKE ?', "%#{query.upcase}%").pluck(:symbol)
   end
+
+  def price
+
+  end
+
+  def circulating_supply
+
+  end
+
+  def max_supply
+
+  end
+
+  def market_cap
+
+  end
+
+  def ath
+
+  end           
 end
