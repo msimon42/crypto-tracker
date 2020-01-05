4e5120eee4ce81e7d85cfed0deb0f157006bdd4d
msimon42 <msimon@uab.edu>
2020-01-04 22:09:14 -0700

    Add userassets controller

---
+++ app/controllers/user_assets_controller.rb
@@ -0,0 +1,13 @@
+class UserAssetsController < ApplicationController
+  def new
+    symbols = Asset.find_symbols(params[:query])
+    @assets = AssetApi.get_multiple(symbols)
+  end
+
+  def create
+    asset = Asset.find_by(symbol: params[:symbol])
+    current_user.assets << asset
+    redirect_back fallback_location: '/myassets'
+    flash[:success] = "#{asset.name} added."
+  end
+end
