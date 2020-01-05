0b22cddefb381c172c9bf6f467861fdcf409e07d
msimon42 <msimon@uab.edu>
2020-01-04 22:08:25 -0700

    Add method for calling several assets

--- app/models/asset_api.rb
+++ app/models/asset_api.rb
@@ -13,6 +13,13 @@ class AssetApi
     JSON.parse(response)
   end
 
+  def self.get_multiple(assets)
+    api_query = assets.join('%2C')
+    uri = URI("https://api.nomics.com/v1/currencies/ticker?key=2595a8427418356ea4954a8c2f0d2782&ids=#{api_query}")
+    response = Net::HTTP.get(uri)
+    JSON.parse(response)
+  end
+
   def self.get_metadata(asset)
     uri = URI("https://api.nomics.com/v1/currencies?key=2595a8427418356ea4954a8c2f0d2782&ids=#{asset}")
     response = Net::HTTP.get(uri)
