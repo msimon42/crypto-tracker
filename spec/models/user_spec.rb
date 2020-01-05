23380ed3c28e90f5b755df4b1584982b201756ff
msimon42 <msimon@uab.edu>
2020-01-05 00:17:34 -0700

    Add model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,17 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:user_assets) }
   it { should have_many(:assets).through(:user_assets) }
+
+  describe 'methods' do
+    before :each do
+      @user = create :user
+      @assets = create_list :asset, 5
+    end
+
+    it 'has_asset?' do
+      @user.assets << @assets[1]
+      expect(@user.has_asset?(@assets[1].symbol)).to be true
+      expect(@user.has_asset?(@assets[3].symbol)).to be false 
+    end
+  end
 end
