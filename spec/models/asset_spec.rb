83e9f3a2174e587949edff6fe90db36aff0ec49d
msimon42 <msimon@uab.edu>
2020-01-05 22:55:17 -0700

    Add test

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Asset, type: :model do
   it { should have_many(:user_assets) }
   it { should have_many(:users).through(:user_assets) }
+
+  describe 'methods' do
+    before :each do
+      @asset = create :asset
+    end
+
+    it 'price' do
+      expect(@asset.attributes[:price]).to eq('123')
+    end
+
+  end
 end
