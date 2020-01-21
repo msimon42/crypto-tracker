6b9ca091f9d6a3ef164a5d548e2b73691b6d4b34
msimon42 <msimon@uab.edu>
2020-01-20 17:29:50 -0700

    Add relationships

--- app/models/user_asset.rb
+++ app/models/user_asset.rb
@@ -1,8 +1,24 @@
 class UserAsset < ApplicationRecord
   belongs_to :user
   belongs_to :asset
+  has_many :trades
 
   def value
     amount * asset.attributes[:price].to_f
+  end
+
+  def name
+    asset.name.titleize
+  end
+
+  def price_per_coin
+    asset.attributes[:price]
+  end
+
+  def amount
+    bought = trades.where(transaction_type: 0).sum(:amount)
+    sold = trades.where(transaction_type: 1).sum(:amount)
+
+    bought - sold
   end
 end
