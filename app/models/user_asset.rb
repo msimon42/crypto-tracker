463ec9810d7c944059c231a181d52446b6729eee
msimon42 <msimon@uab.edu>
2020-01-20 19:26:08 -0700

    Add methods to get portfolio values

--- app/models/user_asset.rb
+++ app/models/user_asset.rb
@@ -12,7 +12,7 @@ class UserAsset < ApplicationRecord
   end
 
   def price_per_coin
-    asset.attributes[:price]
+    asset.attributes[:price].to_f
   end
 
   def amount
@@ -20,5 +20,26 @@ class UserAsset < ApplicationRecord
     sold = trades.where(transaction_type: 1).sum(:amount)
 
     bought - sold
+  end
+
+  def percent_holdings
+    value / user.user_assets.total_value
+  end
+
+  def one_day_change
+    asset.attributes[:price_changes][:one_d].to_f * amount
+  end
+
+  def value
+    amount * price_per_coin
+  end
+
+  def self.total_value
+    all.sum { |ua| ua.value }
+  end
+
+  def self.percent_change
+    usd_change = all.map { |ua| ua.one_day_change }.sum
+    (usd_change / (total_value - usd_change)) * 100
   end
 end
