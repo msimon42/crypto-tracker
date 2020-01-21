068715222434b18b959c96e8688cd513a306c59d
msimon42 <msimon@uab.edu>
2020-01-21 01:52:11 -0700

    Add method to get cost basis

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,5 +1,16 @@
 class Trade < ApplicationRecord
   belongs_to :user_asset
   enum transaction_type:['buy', 'sell']
-  
+
+  def basis
+    amount * price_per_coin
+  end
+
+  def change_since_transaction(current_price)
+    ((current_price - price_per_coin) / price_per_coin) * 100
+  end
+
+  def asset
+    user_asset.asset
+  end
 end
