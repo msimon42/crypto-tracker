556b9b11416673337f0e899a82178ed82035c238
msimon42 <msimon@uab.edu>
2020-01-23 02:54:43 -0700

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Trade.destroy_all
+UserAsset.destroy_all
+User.destroy_all
 Asset.destroy_all
 assets = AssetApi.get_all
 assets.each do |asset|
