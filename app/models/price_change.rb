0c5953812079c198a90aed07f3cb7f5de6e80d5d
msimon42 <msimon@uab.edu>
2020-03-10 23:09:02 -0600

    Add price_change facade

---
+++ app/models/price_change.rb
@@ -0,0 +1,14 @@
+class PriceChange
+  attr_reader :one_d,
+              :seven_d,
+              :thirty_d,
+              :one_y
+
+  def initialize(data)
+    @data = data
+    @one_d = data['1d']['price_change'] || 0
+    @seven_d = data['7d']['price_change'] || 0
+    @thirty_d = data['30d']['price_change'] || 0
+    @one_y = data['365d']['price_change'] || 0
+  end
+end
