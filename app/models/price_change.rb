79c8c11f300d79adc008f364900f076e613ee494
msimon42 <msimon@uab.edu>
2020-03-10 23:27:27 -0600

    Add ytd and 24hr volume

--- app/models/price_change.rb
+++ app/models/price_change.rb
@@ -10,5 +10,7 @@ class PriceChange
     @seven_d = data['7d']['price_change'] || 0
     @thirty_d = data['30d']['price_change'] || 0
     @one_y = data['365d']['price_change'] || 0
+    @ytd = data['ytd']['price_change'] || 0
+    @volume = data['1d']['volume'] || 0  
   end
 end
