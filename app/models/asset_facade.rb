0ab32539719fc2e7f6aca5b1b51fa664a125ef55
msimon42 <msimon@uab.edu>
2020-03-10 23:17:40 -0600

    Add metadata to asset facade

--- app/models/asset_facade.rb
+++ app/models/asset_facade.rb
@@ -4,15 +4,24 @@ class AssetFacade
               :max_supply,
               :market_cap,
               :ath,
-              :price_changes
+              :price_changes,
+              :reddit_url,
+              :github_url,
+              :telegram_url,
+              :website_url
 
   def initialize(symbol)
     @data = AssetService.new.get(symbol).first
+    @metadata = AssetService.new.get_metadata(symbol).first
     @price = @data['price']
     @circulating_supply = @data['circulating_supply']
     @max_supply = @data['max_supply']
     @market_cap = @data['market_cap']
     @ath = @data['high']
     @price_changes = PriceChange.new(@data)
-  end 
+    @reddit_url = @metadata['reddit_url']
+    @github_url = @metadata['github_url']
+    @telegram_url = @metadata['telegram_url']
+    @website_url = @metadata['website_url']
+  end
 end
